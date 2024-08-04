221b9cb4d8b8d3863b48df20e902f9c508aca7d4
Gregory Linscheid <linscheid.greg@gmail.com>
2024-08-04 02:19:23 -0700

    :wrench: Formatting

--- plugin/media-controls.lua
+++ plugin/media-controls.lua
@@ -1,5 +1,5 @@
 if vim.fn.has("nvim-0.7.0") ~= 1 then
-   vim.api.nvim_err_writeln("media-controls.nvim requires at least nvim-0.7.0.")
+   vim.api.nvim_err_writeln("vilos92/media-controls.nvim requires at least nvim-0.7.0.")
 end
 
 if _G.loaded_music_controls then
