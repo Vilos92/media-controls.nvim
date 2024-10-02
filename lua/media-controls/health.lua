f9fdbadfa5b32c7ea551ce69ffbf71e3b0363004
Gregory Linscheid <linscheid.greg@gmail.com>
2024-10-01 22:46:19 -0700

    :stethoscope: checkhealth for media-controls

---
+++ lua/media-controls/health.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+local function check_nowplaying_cli()
+  return vim.fn.executable("nowplaying-cli") == 1
+end
+
+M.check = function()
+  vim.health.start("media-controls report")
+  if check_nowplaying_cli() then
+    vim.health.ok("nowplaying-cli is installed")
+  else
+    vim.health.error("nowplaying-cli is not installed")
+  end
+end
+return M
